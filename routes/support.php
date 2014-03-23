2a5834ae46992b80ffa7213168cad845737811c5
DashTec-Services <info@dashtec.de>
2014-03-23 19:38:19 +0100

    Support

--- routes/support.php
+++ routes/support.php
@@ -18,20 +18,13 @@
  *      Admin Routes
  *
  */
-// anzeigen der gesamten tickets
-$app->get('/support/list', function() use ($app){
-    $SPMenu = new SP\Menu\MenuInclusion();
-    $SPMenu->MenuInclude($app);
-    $supportTickets = DB::query("SELECT * FROM support WHERE user_id != 0");
-    $app->render('support/supportlist.phtml', compact('supportTickets'));
-})->name('support');
-
 
 /*
  *
  *      User Routes
  *
  */
+// anzeigen der gesamten tickets
 $app->get('/support/list', function() use ($app){
     $SPMenu = new SP\Menu\MenuInclusion();
     $SPMenu->MenuInclude($app);
@@ -39,11 +32,35 @@ $app->get('/support/list', function() use ($app){
     $app->render('support/supportuserlist.phtml', compact('supportTickets'));
 })->name('support');
 
+
 $app->get('/support/addticket', function() use ($app){
     $SPMenu = new SP\Menu\MenuInclusion();
     $SPMenu->MenuInclude($app);
     $app->render('support/addticket.phtml');
+
+
 })->name('support');
+
+$app->post('/support/addticket', function() use ($app){
+    $SPMenu = new SP\Menu\MenuInclusion();
+    $SPMenu->MenuInclude($app);
+    if (isset($_POST['eintragen'])) {
+        $form = new \core\postget\postgetcoll();
+        $formData[] = $form->collvars('POST');
+        // TODO: Datum bis wann angezeigt wird.
+        $formData['0']['add_time'] = date("Y-m-d");
+        $formData['0']['status'] = 'Offen';
+        $formData['0']['user_id'] = $_SESSION['account_id'];
+        unset($formData['0']['eintragen']);
+        \DB::insert('support', $formData);
+        $sp_growl = new core\sp_special\growl();
+        $sp_growl->writeGrowl('success', _('Send Reply'), '');
+    }
+    $app->render('support/addticket.phtml');
+})->name('support');
+
+
+
 
 
 $app->post('/support/chticket', function () use ($app) {
@@ -64,19 +81,29 @@ $app->post('/support/chticket', function () use ($app) {
     }
 
 
-
-
-
-
-
-
-
     $SPMenu = new SP\Menu\MenuInclusion();
     $SPMenu->MenuInclude($app);
     $supportTickets = DB::query("SELECT * FROM support WHERE user_id != 0");
     $app->render('support/supportlist.phtml', compact('supportTickets'));
 })->name('doLogin');
 
+
+$app->post('/support/list', function () use ($app) {
+
+    $changer = explode(".", $_POST['delentry']);
+
+    if ($changer['0'] == 'del'){
+        DB::delete('support', "id=%s", $changer['1']);
+        $SPMenu = new SP\Menu\MenuInclusion();
+        $SPMenu->MenuInclude($app);
+        $supportTickets = DB::query("SELECT * FROM support WHERE user_id != 0");
+        $app->render('support/supportlist.phtml', compact('supportTickets'));
+    }
+
+
+
+
+})->name('doLogin');
 
 
 
