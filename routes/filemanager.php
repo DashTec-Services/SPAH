f44dc647951c8cd544d442da73860f6886e3c328
DashTec-Services <info@dashtec.de>
2014-03-25 20:08:38 +0100

    Playlist Add, Del,

--- routes/filemanager.php
+++ routes/filemanager.php
@@ -13,3 +13,74 @@
  * Lizenz: http://login.streamerspanel.de/user/terms
  */
 
+$app->get('/filemanager/list', function () use ($app) {
+    $SPMenu = new SP\Menu\MenuInclusion();
+    $SPMenu->MenuInclude($app);
+    $app->render('filemanager/list.phtml', compact('Users'));
+})->name('list');
+
+$app->get('/filemanager/playlist', function () use ($app) {
+    $SPMenu = new SP\Menu\MenuInclusion();
+    $SPMenu->MenuInclude($app);
+    $app->render('filemanager/playlist.phtml', compact('Users'));
+})->name('list');
+
+$app->get('/filemanager/upload', function () use ($app) {
+    $SPMenu = new SP\Menu\MenuInclusion();
+    $SPMenu->MenuInclude($app);
+    $app->render('filemanager/upload.phtml', compact('Users'));
+})->name('list');
+
+$app->get('/filemanager/playlist/edit', function () use ($app) {
+    $SPMenu = new SP\Menu\MenuInclusion();
+    $SPMenu->MenuInclude($app);
+    $app->render('filemanager/editplaylist.phtml', compact('Users'));
+})->name('list');
+
+$app->post('/filemanager/playlist', function () use ($app) {
+    if (isset($_POST['pllstaction'])){
+        $changer = explode("#",$_POST['pllstaction']);
+
+        if($changer['0'] == 'del'){
+            DB::delete('playlist', "id=%s", $changer['1']);
+            $SPMenu = new SP\Menu\MenuInclusion();
+            $SPMenu->MenuInclude($app);
+            $app->render('filemanager/playlist.phtml', compact('Users'));
+        }
+
+        if($changer['0'] == 'activ'){
+            $_SESSION['playlistactiv'] = $changer['1'];
+            $SPMenu = new SP\Menu\MenuInclusion();
+            $SPMenu->MenuInclude($app);
+            $app->render('filemanager/editplaylist.phtml', compact('Users'));
+        }
+
+        if($changer['0'] == 'save'){
+            unset($_SESSION['playlistactiv']);
+            $SPMenu = new SP\Menu\MenuInclusion();
+            $SPMenu->MenuInclude($app);
+            $app->render('filemanager/playlist.phtml', compact('Users'));
+        }
+
+
+    }
+
+    if (isset($_POST['addplaylst']) && !empty($_POST['playlist_name'])){
+        DB::insert('playlist', array(
+            'playlist_name' => $_POST['playlist_name'],
+            'add_date' => date('Y-m-d'),
+            'user_id' => $_SESSION['account_id']
+        ));
+        $SPMenu = new SP\Menu\MenuInclusion();
+        $SPMenu->MenuInclude($app);
+        $app->render('filemanager/playlist.phtml', compact('Users'));
+    }
+
+    if(isset($_POST['savepllst'])){
+        $SPMenu = new SP\Menu\MenuInclusion();
+        $SPMenu->MenuInclude($app);
+        $app->render('filemanager/playlist.phtml', compact('Users'));
+    }
+
+})->name('doLogin');
+
